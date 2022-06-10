fe66c89dd20ea53126433fd12312ad269dfafc98
GiacomoOffre <giacomo.offre@gmail.com>
2022-06-10 19:55:51 +0200

    no message

---
+++ Assets/Scripts/eating.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class eating : MonoBehaviour
+{
+    public RawImage donuteater;
+    private int nCiambelle = 7;
+
+    public RawImage ciambella00;
+    public RawImage ciambella01;
+    public RawImage ciambella02;
+    public RawImage ciambella03;
+    public RawImage ciambella04;
+    public RawImage ciambella05;
+    public RawImage ciambella06;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+           
+        }
+        
+    }
+}

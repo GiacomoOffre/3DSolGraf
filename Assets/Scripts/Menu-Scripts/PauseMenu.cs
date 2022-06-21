0ff304619aedaad266d029b0599687f7e93b7e6a
GiacomoOffre <giacomo.offre@gmail.com>
2022-06-21 19:48:32 +0200

    fix ause menu 1

--- Assets/Scripts/Menu-Scripts/PauseMenu.cs
+++ Assets/Scripts/Menu-Scripts/PauseMenu.cs
@@ -25,12 +25,14 @@ public class PauseMenu : MonoBehaviour
     }
     public void Resume()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
     void Pause()
     {
+        Cursor.lockState = CursorLockMode.None;
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;

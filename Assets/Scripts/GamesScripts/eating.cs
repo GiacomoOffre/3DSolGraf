cb2140558b0d5a1d72b2c84e43f3077addc57266
GiacomoOffre <giacomo.offre@gmail.com>
2022-06-24 19:39:58 +0200

    due minigiochi base

--- Assets/Scripts/GamesScripts/eating.cs
+++ Assets/Scripts/GamesScripts/eating.cs
@@ -5,32 +5,41 @@ using UnityEngine.UI;
 
 public class eating : MonoBehaviour
 {
-    public RawImage donuteater;
-    private int nCiambelle = 7;
+    public GameObject donuteater;
+    private int nCiambelle = 0;
 
-    public RawImage ciambella00;
-    public RawImage ciambella01;
-    public RawImage ciambella02;
-    public RawImage ciambella03;
-    public RawImage ciambella04;
-    public RawImage ciambella05;
-    public RawImage ciambella06;
+    public GameObject ciambella00;
+    public GameObject ciambella01;
+    public GameObject ciambella02;
+    public GameObject ciambella03;
+    public GameObject ciambella04;
+    public GameObject ciambella05;
+    public GameObject ciambella06;
 
+    private GameObject[] ciambelle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ciambelle = new GameObject[7];
+        ciambelle[0] = ciambella06;
+        ciambelle[1] = ciambella05;
+        ciambelle[2] = ciambella04;
+        ciambelle[3] = ciambella03;
+        ciambelle[4] = ciambella02;
+        ciambelle[5] = ciambella01;
+        ciambelle[6] = ciambella00;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && nCiambelle<7)
         {
-
-           
+            donuteater.transform.position = ciambelle[nCiambelle].transform.position;
+            ciambelle[nCiambelle].active = false;
+            nCiambelle = nCiambelle +1;            
         }
         
     }
---
+++ Assets/Scripts/GamesScripts/gelato.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gelato : MonoBehaviour
+{
+    public GameObject cono;
+    public GameObject crema;
+    public GameObject fragola;
+    public GameObject cioccolato;
+
+    public GameObject[] gelati;
+
+    private int n = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gelati = new GameObject[3];
+        gelati[0] = crema;
+        gelati[1] = fragola;
+        gelati[2] = cioccolato;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Jump") && n <3)
+        {
+            if (n == 0)
+            {
+                gelati[n].transform.position = cono.transform.position + new Vector3(0, 210, 0);
+                n = n + 1;
+            } else if (n == 1)
+            {
+                gelati[n].transform.position = cono.transform.position + new Vector3(0, 320, 0);
+                n = n + 1;
+            }else if (n == 2)
+            {
+                gelati[n].transform.position = cono.transform.position + new Vector3(0, 440, 0);
+                n = n + 1;
+            }
+        }
+
+    }
+}
